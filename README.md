d43353d124292be9432a74ce80cc1e6191b675d4
Dennis Hedback <d.hedback@gmail.com>
2022-09-25 17:08:07 +0200

    Squashed commit of the following:
    - Add dependency and description to setupy.py
    - Add licensing information to README

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # benkpress-api
 
 Small API to simplify the use of sklearn pipelines whose
-training data have been built with the help of benkpress,
+training data have been built with the help of
+[benkpress](https://github.com/dennishedback/benkpress),
 without having to pull in all of benkpress as a dependency.
+benkpress-api also uses a more permissive (BSD 2-clause) license
+than benkpress proper does (GPLv3). 
--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ from setuptools import setup
 setup(
     name="benkpress_api",
     version="0.1.0",
-    description="<one line to give the program's name and a brief idea of what it does.>",
+    description="API for interoperating with benkpress.",
     long_description=open(
         os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
     ).read(),
     long_description_content_type="text/markdown",
     author="Dennis Hedback",
     packages=["benkpress_api"],
-    install_requires=[],
+    install_requires=["sklearn"],
 )
